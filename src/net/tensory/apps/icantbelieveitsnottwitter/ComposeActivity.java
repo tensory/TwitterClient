1fe33750d7661a37932a68cf5cd4c8887213b7c2
Ari Lacenski <alacenski@gmail.com>
2013-09-07 11:33:29 -0700

    Added button layout.

--- src/net/tensory/apps/icantbelieveitsnottwitter/ComposeActivity.java
+++ src/net/tensory/apps/icantbelieveitsnottwitter/ComposeActivity.java
@@ -1,15 +1,21 @@
 package net.tensory.apps.icantbelieveitsnottwitter;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.view.Menu;
 
 public class ComposeActivity extends Activity {
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_compose);
+		
+		ActionBar actionBar = getActionBar();
+	    actionBar.setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
@@ -19,4 +25,11 @@ public class ComposeActivity extends Activity {
 		return true;
 	}
 
+	// Suppress transition animation on Back press.
+	// From http://stackoverflow.com/questions/6959043/cannot-disable-transition-animation-when-back-button-is-clicked
+	@Override
+	public void onPause() {
+		super.onPause();
+		overridePendingTransition(0, 0);
+	}
 }
