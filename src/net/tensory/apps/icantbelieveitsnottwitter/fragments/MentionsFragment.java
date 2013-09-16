a50a79a8ab488b177b3176709cd950003b03a059
Ari Lacenski <alacenski@gmail.com>
2013-09-16 14:04:33 -0700

    Adding mentions view that will consume the TweetListFragment

---
+++ src/net/tensory/apps/icantbelieveitsnottwitter/fragments/MentionsFragment.java
@@ -0,0 +1,11 @@
+package net.tensory.apps.icantbelieveitsnottwitter.fragments;
+
+import android.os.Bundle;
+
+public class MentionsFragment extends TweetsListFragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
